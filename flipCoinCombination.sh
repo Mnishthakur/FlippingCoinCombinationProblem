e3f55a313893850c6bd7e31bbe4a1f32a7e65854
“Mnishthakur” <mnish771@gmail.com>
2023-03-06 20:06:24 +0530

    Add: as a simulator loop to create a doublet combination. Store the doublet combination as well as the percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,28 +1,42 @@
 num_flips=100
 
-# Initialize the dictionary to store the singlet combinations
-declare -A singlets=([heads]=0 [tails]=0)
+# Initialize the dictionary to store the doublet combinations
+declare -A doublets=([HH]=0 [HT]=0 [TH]=0 [TT]=0)
 
-# Loop through the flips and count the singlet combinations
+# Loop through the flips and count the doublet combinations
 for (( i=1; i<=$num_flips; i++ ))
 do
-    # Generate a random number between 0 and 1
-    random_number=$((RANDOM % 2))
+    # Generate a random number between 0 and 3
+    random_number=$((RANDOM % 4))
 
-    # Determine if the result is heads or tails
-    if [[ $random_number -eq 0 ]]; then
-        singlet="heads"
+    # Determine the result of the first coin
+    if [[ $random_number -eq 0 || $random_number -eq 1 ]]; then
+        coin1="H"
     else
-        singlet="tails"
+        coin1="T"
     fi
 
-    # Increment the count of the singlet combination in the dictionary
-    ((singlets[$singlet]++))
+    # Generate another random number between 0 and 1
+    random_number=$((RANDOM % 2))
+
+    # Determine the result of the second coin based on the result of the first coin
+    if [[ $coin1 == "H" && $random_number -eq 0 ]]; then
+        doublet="HH"
+    elif [[ $coin1 == "H" && $random_number -eq 1 ]]; then
+        doublet="HT"
+    elif [[ $coin1 == "T" && $random_number -eq 0 ]]; then
+        doublet="TH"
+    else
+        doublet="TT"
+    fi
+
+    # Increment the count of the doublet combination in the dictionary
+    ((doublets[$doublet]++))
 done
 
-# Calculate the percentage of each singlet combination
-for singlet in "${!singlets[@]}"
+# Calculate the percentage of each doublet combination
+for doublet in "${!doublets[@]}"
 do
-    percentage=$(bc <<< "scale=2; ${singlets[$singlet]} / $num_flips * 100")
-    echo "Percentage of $singlet: $percentage%"
+    percentage=$(bc <<< "scale=2; ${doublets[$doublet]} / $num_flips * 100")
+    echo "Percentage of $doublet: $percentage%"
 done
